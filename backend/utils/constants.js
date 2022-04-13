39cd374d449bacf84056489a67605f885c2a6ec1
Ekaterina Yadrikhinskaya <ya78087@yandex.ru>
2022-04-14 01:37:44 +0300

    fix: corsOptions

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     'https://mestokate.nomoredomains.work',
     'http://mestokate.nomoredomains.work',
-    'localhost:3000',
+    'http://localhost:3000',
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
