d52ea8d5a49d84000da4692c5be914e84488f67d
Ekaterina Yadrikhinskaya <ya78087@yandex.ru>
2022-04-13 21:31:36 +0300

    feat: Logger

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
 const cors = require('./middlewares/cors');
 
@@ -19,7 +20,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(cors);
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(cookieParser()); // подключаем парсер кук как мидлвэр
+app.use(requestLogger); // подключаем логгер запросов
+
 app.use('/', router);
+
+app.use(errorLogger); // подключаем логгер ошибок
 app.use(errors());
 app.use(handlerErrors);
 
